25fccd493f7eb1766643be8f67284e0b2120ff59
Nab3r1us <oleg.kostromin2011@gmail.com>
2024-02-04 23:06:29 +0300

    added swagger

--- Program.cs
+++ Program.cs
@@ -2,8 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
