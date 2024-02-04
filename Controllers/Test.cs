1f5ac3a875e666fecbc37cba4ddccfb9fe579ff7
Nab3r1us <oleg.kostromin2011@gmail.com>
2024-02-05 02:04:26 +0300

    remove swagger

--- Controllers/Test.cs
+++ Controllers/Test.cs
@@ -2,13 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SprPhone.Controllers;
 
-[ApiController]
 [Route("[controller]")]
 public class TestController(ILogger<TestController> logger) : ControllerBase
 {
     private readonly ILogger<TestController> _logger = logger;
 
-    [HttpGet(Name = "GetTest")]
+    [HttpGet]
     public string Get(string name="World")
     {
         return $"Hello, {name}!";
--- Program.cs
+++ Program.cs
@@ -6,17 +6,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    options.RoutePrefix = string.Empty;
-});
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
